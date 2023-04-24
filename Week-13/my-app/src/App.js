870a96f08a6a266a9660fa94f5aa18df878db396
Sidhanth Kamble <sidhanthkamble@Sidhanths-MacBook-Pro.local>
2023-04-24 19:13:00 +0530

    Shorten link react assingment

---
+++ Week-13/my-app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Welcome from './Welcome';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+
+     <div className="App">
+        <Welcome/>
+         <Content/>
+        <Footer/>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ Week-13/my-app/src/Welcome.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './welcome.css';
+
+const Welcome = () => {
+
+    return (
+        <> 
+            <div className="Box1">
+             <div className="Box2">
+             <h1>More than just shorter Links</h1>
+             <p>Build your brand's recogination and get detailed insightson how your links re performing</p>
+             </div>   
+
+            <img src="https://raw.githubusercontent.com/MohamedAridah/frontendmentor_url-shortening-api/main/images/illustration-working.svg" height={400}/>
+            </div>
+        </>
+    )
+}
+
+export default Welcome;
